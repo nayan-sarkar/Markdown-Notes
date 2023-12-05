1cc7fd672f952b487160ce48425a4adfc201f10a
Nayan Sarkar <10292012+nayan-sarkar@users.noreply.github.com>
2023-12-05 12:50:53 -0800

    updates:

--- src/pages/AllNotesPage/PostsList.jsx
+++ src/pages/AllNotesPage/PostsList.jsx
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 export default function PostsList({posts}){
 
     return (
-        <div className='posts'>
+        <div className={styles['posts']}>
         {posts.map((post) => (
-            <div key={post.id} className='post'>
+            <div key={post.id} className={styles['post']}>
                 <Link to={`viewpost/${post.id}`}>
                 <p className={styles['post-title']}>{post.title}</p>
                 <p className={styles['post-body']}>{`${post.body.slice(0,70)}...`}</p>
